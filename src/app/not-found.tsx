66b23bceafcb7cc4d21951c3ce14d129069b78c3
galuhsatria <galuhsatria219@gmail.com>
2023-11-19 13:16:01 +0800

    creating not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="max-w-4xl mx-auto text-center my-44">
+      <div className="mb-6">
+        <h1 className="text-5xl text-primary font-bold">404</h1>
+      </div>
+      <p className="mb-2">Maaf, Halaman tidak ditemukan</p>
+      <div className='mt-5'>
+        <Link className="btn px-6 py-2" href={'/'}>
+          Kembali Ke Beranda
+        </Link>
+      </div>
+    </div>
+  );
+}
