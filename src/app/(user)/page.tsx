70634ef995922159810869956b71642e6f47177f
galuhsatria <galuhsatria219@gmail.com>
2023-11-26 22:16:29 +0800

    change animation library to framer motion

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import { motion } from 'framer-motion';
 
 export default function Page() {
   const [activeFaq, setActiveFaq] = useState(null);
@@ -34,34 +35,73 @@ export default function Page() {
           <Image src={'/asset/batik-icon.svg'} width={600} height={100} alt="batik-image" />
         </div>
         <div className="flex md:flex-row justify-between flex-col-reverse md:gap-0 gap-20 items-center max-w-6xl mx-auto px-4 py-8 ">
-          <div data-aos="fade-right">
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              ease: 'easeInOut',
+              duration: 0.5,
+              y: { duration: 0.5 },
+            }}
+          >
             <Image src={'/asset/fitur-ilustration.svg'} width={400} height={800} alt="fitur-image" />
-          </div>
-          <div className="fitur" data-aos="fade-left">
-            <ul className="flex flex-col gap-8">
+          </motion.div>
+          <div className="fitur">
+            <motion.ul
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                ease: 'easeInOut',
+                duration: 0.5,
+                y: { duration: 0.5 },
+              }}
+              className="flex flex-col gap-8"
+            >
               {fitur.map(({ name, icon }) => (
                 <li className="flex items-center gap-4" key={name}>
                   <div className="bg-white px-3 py-2 text-2xl rounded-md">{icon}</div>
                   <p className="text-xl text-white font-medium">{name}</p>
                 </li>
               ))}
-            </ul>
+            </motion.ul>
           </div>
         </div>
       </section>
 
       <section className="mt-10">
         <div>
-          <div className="text-center flex flex-col items-center">
+          <motion.div
+            className="text-center flex flex-col items-center"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{
+              ease: 'easeInOut',
+              duration: 0.5,
+            }}
+          >
             <h1 className="sm:text-2xl font-semibold px-4 text-xl ">Kenapa Mengunakan Kasa Talk?</h1>
             <div className="w-28 mt-2 h-[2px] bg-primary"></div>
-          </div>
+          </motion.div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-6xl mx-auto md:gap-6 px-4 mt-14 mb-16">
-            {benefitUse.map(({ name, description }) => (
-              <div className="p-5 rounded-md bg-white shadow-sm border" data-aos="fade-up" data-aos-duration="300" key={name}>
+            {benefitUse.map(({ name, description }, index) => (
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{
+                  ease: 'easeInOut',
+                  duration: 0.5,
+                  y: { duration: 0.5 },
+                }}
+                className="p-5 rounded-md bg-white shadow-sm border"
+                key={name}
+              >
                 <h2 className="font-semibold text-xl">{name}</h2>
                 <p className="text-slate-600 mt-3">{description}</p>
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
@@ -69,14 +109,33 @@ export default function Page() {
 
       <section className="bg-white border border-b border-t">
         <div>
-          <div className="text-center flex flex-col items-center mt-16">
+          <motion.div
+            className="text-center flex flex-col items-center mt-16"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{
+              ease: 'easeInOut',
+              duration: 0.5,
+            }}
+          >
             <h1 className="sm:text-2xl font-semibold px-4 text-xl ">Peringkat Kontributor</h1>
             <div className="w-28 mt-2 h-[2px] bg-primary"></div>
-          </div>
+          </motion.div>
           <div className="max-w-4xl mx-auto py-6 mt-8 px-2">
             <ul className="flex flex-col gap-6 mx-auto p-5">
               {people.map(({ name, word, imageUrl }, index) => (
-                <li key={index} data-aos="fade-up" data-aos-duration="300">
+                <motion.li
+                  key={index}
+                  initial={{ opacity: 0, y: 50 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
+                  transition={{
+                    ease: 'easeInOut',
+                    duration: 0.5,
+                    y: { duration: 0.5 },
+                  }}
+                >
                   <div className="flex justify-between items-center border-b pb-2">
                     <div className="flex gap-4 items-center">
                       <Image src={imageUrl} width={50} height={40} alt={name} className="rounded-full" />
@@ -84,18 +143,27 @@ export default function Page() {
                     </div>
                     <p className=" text-primary font-semibold">{word} Kata</p>
                   </div>
-                </li>
+                </motion.li>
               ))}
             </ul>
           </div>
-          <div className="text-center max-w-3xl mx-auto pb-6 px-2">
+          <motion.div
+            className="text-center max-w-3xl mx-auto pb-6 px-2"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{
+              ease: 'easeInOut',
+              duration: 0.5,
+            }}
+          >
             <p className="text-sm md:text-base">
               Bergabung dan jadilah kontributor untuk menambahkan kata-kata yang berlum tersedia dan dapatkan hadiah istimewa.{' '}
               <Link href="/login" className="text-primary underline">
                 Bergabung
               </Link>
             </p>
-          </div>
+          </motion.div>
         </div>
       </section>
 
@@ -106,7 +174,18 @@ export default function Page() {
         </div>
         <ul className="max-w-6xl mx-auto flex flex-col gap-4 px-4 mb-16 mt-14">
           {faqs.map(({ question, answer }, index) => (
-            <li key={index} className="faq-item bg-white p-4 rounded-md border transition-all" data-aos="fade-up" data-aos-duration="300">
+            <motion.li
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                ease: 'easeInOut',
+                duration: 0.5,
+                y: { duration: 0.5 },
+              }}
+              key={index}
+              className="faq-item bg-white p-4 rounded-md border cursor-pointer"
+            >
               <div className="faq-question flex justify-between" onClick={() => toggleFaq(index)}>
                 <h3 className="font-semibold text-lg">{question}</h3>
                 <span className="cursor-pointer text-2xl">{activeFaq === index ? <IoIosArrowUp /> : <IoIosArrowDown />}</span>
@@ -116,7 +195,7 @@ export default function Page() {
                   <p className="mt-4">{answer}</p>
                 </div>
               )}
-            </li>
+            </motion.li>
           ))}
         </ul>
       </section>
