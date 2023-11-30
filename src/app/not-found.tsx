0ceb053b44d44c06465d9cebdefd7a297b5f0fec
galuhsatria <galuhsatria219@gmail.com>
2023-11-30 22:40:18 +0800

    update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 export default function NotFound() {
   return (
     <div className="max-w-4xl mx-auto text-center my-44">
@@ -7,7 +5,7 @@ export default function NotFound() {
         <h1 className="text-5xl text-primary font-bold">404</h1>
       </div>
       <p className="mb-2">Maaf, Halaman tidak ditemukan</p>
-      <div className='mt-5'>
+      <div className="mt-5">
         <a className="btn px-6 py-2" href={'/'}>
           Kembali Ke Beranda
         </a>
