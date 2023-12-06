934a29f9adbd991942e73d1d6023d1ca11f754d8
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-12-06 15:41:27 +0800

    add form input kontribusi

--- src/app/(user)/kontribusi/page.tsx
+++ src/app/(user)/kontribusi/page.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 type EventHandler = (event: React.ChangeEvent<HTMLInputElement>) => void;
 
 export default function Page() {
-  const [kataIndo, setKataIndo] = useState("");
-  const [kataSasak, setKataSasak] = useState("");
-  const [penggunaanKata, setPenggunaanKata] = useState("");
+  const [kataIndo, setKataIndo] = useState<string>("");
+  const [kataSasak, setKataSasak] = useState<string>("");
+  const [penggunaanKataIndo, setPenggunaanKataIndo] = useState<string>("");
+  const [penggunaanKataSasak, setPenggunaanKataSasak] = useState<string>("");
+  const [file, setFile] = useState<File>();
 
   const onHandlerKataIndo: EventHandler = (event) => {
     setKataIndo(event.target.value);
@@ -16,17 +18,44 @@ export default function Page() {
     setKataSasak(event.target.value);
   };
 
-  const onHandlerPenggunaanKata: EventHandler = (event) => {
-    setPenggunaanKata(event.target.value);
+  const onHandlerPenggunaanKataIndo: EventHandler = (event) => {
+    setPenggunaanKataIndo(event.target.value);
+  };
+
+  const onHandlerPenggunaanKataSasak: EventHandler = (event) => {
+    setPenggunaanKataSasak(event.target.value);
+  };
+
+  const onHandlerFile: EventHandler = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const onSubmitKata = async (event: any) => {
+    event.preventDefault();
+    const formAudio = new FormData();
+    formAudio.append("audio", file);
+
+    const body = {
+      kataIndo,
+      kataSasak,
+      penggunaanKataIndo,
+      penggunaanKataSasak,
+    };
+    alert("test");
   };
 
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <form className="max-w-2xl mx-auto space-y-6">
-        <h1 className="text-primary text-2xl font-semibold">Silakan Tambah Kosa Kata Bahasa Sasak</h1>
-        <div className="space-y-3"> 
+      <form className="max-w-2xl mx-auto space-y-6" onSubmit={onSubmitKata}>
+        <h1 className="text-primary text-2xl font-semibold">
+          Silakan Tambah Kosa Kata Bahasa Sasak
+        </h1>
+        <div className="space-y-3">
           <h2 className="text-md">Bahasa Indonesia</h2>
           <input
+            required
             className="rounded-md text-lg px-4 h-10 w-full border-2 focus:outline-primary"
             type="text"
             placeholder="Makan"
@@ -34,9 +63,10 @@ export default function Page() {
             value={kataIndo}
           />
         </div>
-        <div className="space-y-3"> 
+        <div className="space-y-3">
           <h2 className="text-md">Bahasa Sasak</h2>
           <input
+            required
             className="rounded-md text-lg px-4 h-10 w-full border-2 focus:outline-primary"
             type="text"
             placeholder="Medaran"
@@ -44,20 +74,45 @@ export default function Page() {
             value={kataSasak}
           />
         </div>
-        <div className="space-y-3"> 
-          <h2 className="text-md">Contoh Penggunaan Kata</h2>
+        <div className="space-y-3">
+          <h2 className="text-md">
+            Contoh Penggunaan Kata Dalama Bahasa Indonesia
+          </h2>
           <input
+            required
+            className="rounded-md text-lg px-4 h-10 w-full border-2 focus:outline-primary"
+            type="text"
+            placeholder="Kamu Sudah Makan?"
+            onChange={onHandlerPenggunaanKataIndo}
+            value={penggunaanKataIndo}
+          />
+        </div>
+        <div className="space-y-3">
+          <h2 className="text-md">
+            Contoh Penggunaan Kata Dalama Bahasa Sasak
+          </h2>
+          <input
+            required
             className="rounded-md text-lg px-4 h-10 w-full border-2 focus:outline-primary"
             type="text"
             placeholder="Wah Side Medaran?"
-            onChange={onHandlerPenggunaanKata}
-            value={penggunaanKata}
+            onChange={onHandlerPenggunaanKataSasak}
+            value={penggunaanKataSasak}
+          />
+        </div>
+        <div className="space-y-3">
+          <h2 className="text-md">Masukan Suara Kata</h2>
+          <input
+            required
+            className="rounded-md text-lg px-4 h-10 w-full border-2 focus:outline-primary"
+            type="file"
+            onChange={onHandlerFile}
+            value={file}
           />
         </div>
         <button
           type="submit"
           className="btn font-medium h-10 w-full bg-primary text-white">
-          {" "}
           Submit
         </button>
       </form>
