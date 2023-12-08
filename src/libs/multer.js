cfd4d8be32fcd0c121750bf3a89e81fe5929d9fb
galuhsatria <galuhsatria219@gmail.com>
2023-12-08 23:15:56 +0800

    delete config for upload avatar

--- src/libs/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from 'multer';
-import { storage as _storage } from '../config/firebase.config';
-
-const bucket = _storage().bucket();
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-export { upload, bucket };
