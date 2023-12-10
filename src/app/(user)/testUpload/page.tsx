e73283cd9be67b5b899acf41e1a0f6691885036b
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-12-10 12:14:48 +0800

    cancal uploadthing component

--- src/app/(user)/testUpload/page.tsx
+++ src/app/(user)/testUpload/page.tsx
@@ -1,23 +1,9 @@
 "use client";
 
-import { UploadButton } from "@/utils/uploadthing";
-
 const Page = () => {
   return (
     <div>
       <p>upload</p>
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res[0].url);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
     </div>
   );
 };
