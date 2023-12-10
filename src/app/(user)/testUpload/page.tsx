e58d9bfe2fc2ce5fe709567de996b2feea8a33e6
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-12-10 16:10:04 +0800

    uploadfile to firabase

--- src/app/(user)/testUpload/page.tsx
+++ src/app/(user)/testUpload/page.tsx
@@ -1,9 +1,35 @@
 "use client";
 
+import { useState } from "react";
+import { analytics } from "@/app/firebase/firebase-config";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 const Page = () => {
+  const [file, setFile] = useState(null);
+
+  const submit = async (event: any) => {
+    event.preventDefault();
+    console.log(file);
+
+    if (file) {
+      const fileRef = ref(analytics, `kasa-talk-audio/${file.name}`);
+      uploadBytes(fileRef, file).then((data) => {
+        getDownloadURL(data.ref).then((url) => {
+          console.log("url", url);
+        });
+      });
+    } else {
+      alert("belum ada file");
+    }
+  };
   return (
     <div>
-      <p>upload</p>
+      <input
+        type="file"
+        accept=".mp3"
+        onChange={(event: any) => setFile(event.target.files[0])}
+      />
+      <button onClick={submit}>Submit</button>
     </div>
   );
 };
