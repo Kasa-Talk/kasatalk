0dff5a8cd2a05fcdf1f2ab2014a33e92fd556793
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2023-12-11 21:45:34 +0800

    migrate secret key firebase to .env

--- src/app/firebase/firebase-config.js
+++ src/app/firebase/firebase-config.js
@@ -2,21 +2,28 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "@firebase/storage"
+
+import { config } from "dotenv";
+
+config();
+
 // TODO: Add SDKs for Firebase products that you wa nt to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCaCmVSZaDrCOt4WxIhDWxwF7xGae-X2o",
-  authDomain: "uploadfile-92b82.firebaseapp.com",
-  projectId: "uploadfile-92b82",
-  storageBucket: "uploadfile-92b82.appspot.com",
-  messagingSenderId: "143182469488",
-  appId: "1:143182469488:web:e4a599202c2c5808ab17b5",
-  measurementId: "G-1P322B15J8"
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+  measurementId: `${process.env.MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const analytics = getStorage(app);
+
+console.log(process.env.APP_ID);
